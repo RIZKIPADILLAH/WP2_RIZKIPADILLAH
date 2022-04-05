9fcd893b1d27fd05aabea08506a38ea38750997e
WP2_RIZKIPADILLAH <farel.badut@gmail.com>
2022-04-04 21:31:09 -0800

    menambahkan tugas baru

--- application/views/v_about.php
+++ application/views/v_about.php
@@ -2,17 +2,17 @@
     <h1><?php echo $judul ?></h1>
     <h4>Nama</h4>
     <ul type="disc">
-        <li>Nama Depan : Dony</li>
-        <li>Nama Belakang : Ama</li>
+        <li>Nama Depan : Rizki</li>
+        <li>Nama Belakang : Fadillah</li>
     </ul>
     <br>
     <h4>Alamat</h4>
     <ul type="none">
-        <li> Jalan maulana hasanuddin poris gaga</li>
+        <li> Jalan sunan giri</li>
     </ul>
     <h4>Tempat Lahir</h4>
     <ul type="none">
-        <li>NTT</li>
+        <li>Tanggerang</li>
     </ul>
     <h4>Olah Raga Favorit</h4>
     <ul type="square">
